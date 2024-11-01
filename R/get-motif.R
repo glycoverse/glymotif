db4eea453b688ed8f899cdaa2f8ad01e6ebfdd77
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-11-01 14:20:27 +0800

    feat: add `get_motif()`

--- R/get-motif.R
+++ R/get-motif.R
@@ -3,7 +3,37 @@
 #' This function returns the names of the available motifs available in the package.
 #'
 #' @return A character vector of motif names.
+#'
+#' @examples
+#' available_motifs()[1:5]
+#'
 #' @export
 available_motifs <- function() {
   glygen_motifs$name
+}
+
+
+#' Get a Motif Graph
+#'
+#' This function returns a motif graph for a given motif name.
+#' It is handy if the motif is a known motif in GlycoMotif GlyGen Collection.
+#' To create custom motif graphs, use [glyparse::parse_iupac_condensed()].
+#'
+#' @param name A character string of the motif name.
+#' @return A `glycan_graph` object.
+#'
+#' @examples
+#' get_motif("N-Glycan core basic")
+#' get_motif("O-Glycan core 1")
+#'
+#' @export
+get_motif <- function(name) {
+  if (!name %in% glygen_motifs$name) {
+    cli::cli_abort("Unknown motif: {.val {name}}.")
+  }
+  iupac <- glygen_motifs$iupac[glygen_motifs$name == name]
+  graph <- glyparse::parse_iupac_condensed(iupac)
+  aglycon <- glygen_motifs$aglycon[glygen_motifs$name == name]
+  alignment <- glygen_motifs$alignment[glygen_motifs$name == name]
+  list(graph = graph, aglycon = aglycon, alignment = alignment)
 }
--- tests/testthat/test-get-motif.R
+++ tests/testthat/test-get-motif.R
@@ -1,3 +1,17 @@
 test_that("get available motifs", {
   expect_snapshot(available_motifs())
 })
+
+
+test_that("get known motif", {
+  expect_error(get_motif("bad motif"), 'Unknown motif: "bad motif".')
+})
+
+
+test_that("getting motif works", {
+  result <- get_motif("N-Glycan core basic")
+  expect_length(result, 3)
+  expect_s3_class(result$graph, "glycan_graph")
+  expect_identical(result$aglycon, "Asn")
+  expect_identical(result$alignment, "core")
+})
