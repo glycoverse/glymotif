2c20071218ba91cbe5da94168a8aa405284cb07d
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-11-14 12:52:05 +0800

    feat: add `counts_motif()`

--- R/has-motif.R
+++ R/has-motif.R
@@ -98,7 +98,7 @@
 #'
 #' @return A logical value indicating if the `glycan` has the `motif`.
 #'
-#' @seealso [has_motifs()], [have_motif()], [have_motifs()]
+#' @seealso [has_motifs()], [have_motif()], [have_motifs()], [counts_motif()]
 #'
 #' @examples
 #' library(glyparse)
@@ -161,7 +161,78 @@
 #' @export
 has_motif <- function(glycan, motif, alignment = "substructure", ignore_linkages = FALSE) {
   alignment_provided <- !missing(alignment)
+  params <- prepare_has_motif_args(glycan, motif, alignment, alignment_provided, ignore_linkages)
+  rlang::exec("has_motif_", !!!params)
+}
 
+
+#' Count How Many Times a Glycan has the Given Motif
+#'
+#' This function is closely related to [has_motif()].
+#' However, instead of returning a logical value, it returns the number of times
+#' the `glycan` has the `motif`.
+#'
+#' @inheritParams has_motif
+#'
+#' @details
+#' This function actually perform v2f algorithm to get all possible matches
+#' between `glycan` and `motif`.
+#' However, the result is not necessarily the number of matches.
+#'
+#' Think about the following example:
+#' - glycan: `Gal(b1-?)[Gal(b1-?)]GlcNAc(b1-4)GlcNAc`
+#' - motif: `Gal(b1-?)[Gal(b1-?)]GlcNAc(b1-)`
+#'
+#' To draw the glycan out:
+#' ```
+#' Gal 1
+#'    \ b1-? b1-4
+#'     GlcNAc -- GlcNAc
+#'    / b1-?
+#' Gal 2
+#' ```
+#' To draw the motif out:
+#' ```
+#' Gal 1
+#'    \ b1-?
+#'     GlcNAc b1-
+#'    / b1-?
+#' Gal 2
+#' ```
+#'
+#' To differentiate the galactoses, we number them as "Gal 1" and "Gal 2"
+#' in both the glycan and the motif.
+#' The v2f subisomorphic algorithm will return two matches:
+#' - Gal 1 in the glycan matches Gal 1 in the motif, and Gal 2 matches Gal 2.
+#' - Gal 1 in the glycan matches Gal 2 in the motif, and Gal 2 matches Gal 1.
+#'
+#' However, from a biological perspective, the two matches are the same.
+#' This function will take care of this, and return the "unique" number of matches.
+#'
+#' For other details about the handling of monosaccharide, linkages, alignment,
+#' substituents, and implementation, see [has_motif()].
+#'
+#' @seealso [has_motif()]
+#'
+#' @return An integer indicating how many times the `glycan` has the `motif`.
+#'
+#' @examples
+#' counts_motif("Gal(b1-3)Gal(b1-3)GalNAc", "Gal(b1-")
+#' counts_motif(
+#'   "Man(b1-?)[Man(b1-?)]GalNAc(b1-4)GlcNAc",
+#'   "Man(b1-?)[Man(b1-?)]GalNAc"
+#' )
+#' counts_motif("Gal(b1-3)Gal", "Man")
+#'
+#' @export
+counts_motif <- function(glycan, motif, alignment = "substructure", ignore_linkages = FALSE) {
+  alignment_provided <- !missing(alignment)
+  params <- prepare_has_motif_args(glycan, motif, alignment, alignment_provided, ignore_linkages)
+  rlang::exec("counts_motif_", !!!params)
+}
+
+
+prepare_has_motif_args <- function(glycan, motif, alignment, alignment_provided, ignore_linkages) {
   # Check input arguments
   valid_glycan_arg(glycan)
   valid_motif_arg(motif)
@@ -188,8 +259,7 @@ has_motif <- function(glycan, motif, alignment = "substructure", ignore_linkages
   # an error will be raised by `ensure_glycan_mono_type()`.
   glycan <- ensure_glycan_mono_type(glycan, motif)
 
-  # Check if the glycan has the motif
-  has_motif_(glycan, motif, alignment = alignment, ignore_linkages = ignore_linkages)
+  list(glycan = glycan, motif = motif, alignment = alignment, ignore_linkages = ignore_linkages)
 }
 
 
@@ -240,12 +310,46 @@ valid_ignore_linkages_arg <- function(x) {
 
 has_motif_ <- function(glycan, motif, alignment = "substructure", ignore_linkages = FALSE) {
   # This function is the logic part of `has_motif()`.
-
-  # Colorize the graphs
   c_graphs <- colorize_graphs(glycan, motif)
   glycan <- c_graphs$glycan
   motif <- c_graphs$motif
+  res <- perform_vf2(glycan, motif)
+  any(purrr::map_lgl(
+    res, is_vaild_result, glycan = glycan, motif = motif,
+    alignment = alignment, ignore_linkages = ignore_linkages
+  ))
+}
 
+
+counts_motif_ <- function(glycan, motif, alignment = "substructure", ignore_linkages = FALSE) {
+  # This function is the logic part of `counts_motif()`.
+  c_graphs <- colorize_graphs(glycan, motif)
+  glycan <- c_graphs$glycan
+  motif <- c_graphs$motif
+  res <- perform_vf2(glycan, motif)
+  valid_mask <- purrr::map_lgl(
+    res, is_vaild_result, glycan = glycan, motif = motif,
+    alignment = alignment, ignore_linkages = ignore_linkages
+  )
+  valid_res <- res[valid_mask]
+  count_set_unique(valid_res)
+}
+
+
+count_set_unique <- function(lst) {
+  # Given a list of integer vectors, return the number of unique vectors.
+  # Uniqueness is determined by `setequal()`.
+  unique_list <- list()
+  for (vec in lst) {
+    if (purrr::none(unique_list, ~ setequal(.x, vec))) {
+      unique_list <- append(unique_list, list(vec))
+    }
+  }
+  length(unique_list)
+}
+
+
+perform_vf2 <- function(glycan, motif) {
   # Perform "VF2" algorithm
   # `res` is a list of all possible matches between `glycan` and `motif`.
   # Each match is an integer vector, with the same length as `vcount(motif)`.
@@ -254,14 +358,7 @@ has_motif_ <- function(glycan, motif, alignment = "substructure", ignore_linkage
   # e.g. If `res[[1]] = c(2, 3)`,
   # it means the 1st vertex in `motif` matches the 2nd vertex in `glycan`,
   # and the 2nd vertex in `motif` matches the 3rd vertex in `glycan`.
-  res <- igraph::graph.get.subisomorphisms.vf2(glycan, motif)
-
-  # Check each result in `res` for the following:
-  # alignment, linkages, anomer, substituents
-  any(purrr::map_lgl(
-    res, is_vaild_result, glycan = glycan, motif = motif,
-    alignment = alignment, ignore_linkages = ignore_linkages
-  ))
+  igraph::graph.get.subisomorphisms.vf2(glycan, motif)
 }
 
 
--- tests/testthat/test-has-motif.R
+++ tests/testthat/test-has-motif.R
@@ -448,3 +448,32 @@ test_that("anomer wrong for core motif", {
   motif <- glyparse::parse_iupac_condensed("Gal(b1-4)GlcNAc(a1-")
   expect_false(has_motif(glycan, motif))
 })
+
+
+# ========== Count Motifs ==========
+test_that("count motifs in glycan", {
+  glycan <- glyparse::parse_iupac_condensed("Gal(b1-3)Gal(b1-3)GalNAc")
+  motif <- glyparse::parse_iupac_condensed("Gal(b1-")
+  expect_equal(counts_motif(glycan, motif), 2L)
+})
+
+
+test_that("count motifs with branching", {
+  glycan <- glyparse::parse_iupac_condensed("Man(b1-?)[Man(b1-?)]GalNAc(b1-4)GlcNAc")
+  motif <- glyparse::parse_iupac_condensed("Man(b1-?)[Man(b1-?)]GalNAc")
+  expect_equal(counts_motif(glycan, motif), 1L)
+})
+
+
+test_that("count symmetrical motif", {
+  glycan <- glyparse::parse_iupac_condensed("Gal(b1-3)Gal(b1-3)GalNAc")
+  motif <- glyparse::parse_iupac_condensed("Gal(b1-3)Gal")
+  expect_equal(counts_motif(glycan, motif), 1L)
+})
+
+
+test_that("count 0 motif", {
+  glycan <- glyparse::parse_iupac_condensed("Gal(b1-3)Gal(b1-3)GalNAc")
+  motif <- glyparse::parse_iupac_condensed("Gal(b1-4)GalNAc")
+  expect_equal(counts_motif(glycan, motif), 0L)
+})
